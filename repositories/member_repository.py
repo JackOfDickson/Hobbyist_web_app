67f7caa0d7e644bf639477cffddf02413e5938b9
Jack <jackowendickson@gmail.com>
2022-03-20 19:02:05 +0000

    added member repository

--- repositories/member_repository.py
+++ repositories/member_repository.py
@@ -1,2 +1,37 @@
 from db.run_sql import run_sql
 from models.member import Member
+
+def save(member):
+    sql = "INSERT INTO members (name) VALUES (%s) RETURNING id"
+    values = [member.name]
+    results = run_sql(sql, values)
+    member.id = results[0]['id']
+    return member
+
+def select_all():
+    members = []
+    
+    sql = "SELECT * FROM members"
+    results = run_sql(sql)
+    
+    for row in results:
+        member = Member(row['title'], row['id'])
+        members.append(member)
+    return members
+
+def select(id):
+    member = None
+    
+    sql = "SELECT * FROM members WHERE id = %s"
+    values = [id]
+    result = run_sql(sql, values)[0]
+    
+    if result is not None:
+        member = Lesson(result['title'], result['id'])
+    return member
+
+def delete_all():
+    sql = "DELETE FROM members"
+    run_sql(sql)
+    
+# delete individual id for extension
