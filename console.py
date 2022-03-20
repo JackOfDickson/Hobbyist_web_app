548d5d9e58e09dd411ef136119c5a16134fb78d1
Jack <jackowendickson@gmail.com>
2022-03-20 19:42:41 +0000

    fixed booking repository bug

--- console.py
+++ console.py
@@ -1,11 +1,13 @@
+from models.booking import Booking
 from models.lesson import Lesson
 from models.member import Member
 
-
+import repositories.booking_repository as booking_repository
 import repositories.lesson_repository as lesson_repository
 import repositories.member_repository as member_repository
 
 
+booking_repository.delete_all()
 lesson_repository.delete_all()
 member_repository.delete_all()
 
@@ -15,3 +17,5 @@ lesson_repository.save(lesson1)
 
 member1 = Member("Carl Marks")
 member_repository.save(member1)
+
+booking1 = Booking(member1, lesson1)
