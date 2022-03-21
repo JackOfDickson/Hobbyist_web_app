8ac6e95ba107724b4ce5a2260d5bfd0c501405bb
Jack <jackowendickson@gmail.com>
2022-03-21 13:10:33 +0000

    added a couple of routes to member controller for creating a new member

--- controllers/member_controller.py
+++ controllers/member_controller.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, Blueprint, redirect, request
 import repositories.member_repository as member_repository
 
+from models.member import Member
+
 
 members_blueprint = Blueprint("members", __name__)
 
@@ -8,3 +10,15 @@ members_blueprint = Blueprint("members", __name__)
 def members():
     members = member_repository.select_all()
     return render_template("members/index.html", members = members)
+
+@members_blueprint.route("/members/new", methods = ['GET'])
+def new_member():
+    return render_template ('members/new.html')
+
+@members_blueprint.route("/members", methods=['POST'])
+def create_user():
+    name = request.form['name']
+    member = Member(name)
+    member_repository.save(member)
+    return redirect ('/members')
+    
