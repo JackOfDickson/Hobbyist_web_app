47a3ccb86a4d50c510a6c7af3d2aad2845dfe2f7
Jack <jackowendickson@gmail.com>
2022-03-21 13:48:05 +0000

    created a couple of routes in the booking controller for the forum setup\'

--- controllers/booking_controller.py
+++ controllers/booking_controller.py
@@ -1,2 +1,28 @@
 from flask import Flask, render_template, Blueprint, redirect, request
+from models.booking import Booking
 import repositories.booking_repository as booking_repository
+import repositories.member_repository as member_repository
+import repositories.lesson_repository as lesson_repository
+
+bookings_blueprint = Blueprint("bookings", __name__)
+
+@bookings_blueprint.route('/bookings')
+def bookings():
+    bookings = booking_repository.select_all()
+    return render_template("bookings.html", bookings = bookings)
+
+@bookings_blueprint.route('/bookings/new', methods = ['GET'])
+def new_booking():
+    members = member_repository.select_all()
+    lessons = lesson_repository.select_all()
+    return render_template("bookings/new.html", members = members, lessons = lessons)
+
+@bookings_blueprint.route('/bookings')
+def create_booking():
+    member_id = request.form['member_id']
+    lesson_id = request.form['lessson_id']
+    member = member_repository.select(member_id)
+    lesson = lesson_repository.select(lesson_id)
+    booking = Booking(member,lesson)
+    booking_repository.save(booking)
+    return redirect('/bookings')
