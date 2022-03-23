786ad8fe9d72f75cbf31ada6e865bf5a23cf4e40
Jack <jackowendickson@gmail.com>
2022-03-23 10:23:50 +0000

    commented out the duplicate booking code, added capacity to lesson edit form

--- repositories/booking_repository.py
+++ repositories/booking_repository.py
@@ -1,9 +1,17 @@
 from db.run_sql import run_sql
 from models.booking import Booking
-import datetime
 
 import repositories.lesson_repository as lesson_repository
 import repositories.member_repository as member_repository
+
+# def check_duplicate_booking(booking):
+#     member = booking.member
+#     lesson_members = lesson_repository.members
+#     for lesson_member in lesson_members:
+#         if member == lesson_member:
+#             return True
+#         else: continue
+#     return False
 
 def check_capacity(booking):
     lesson = booking.lesson
@@ -14,9 +22,12 @@ def check_capacity(booking):
         return True
 
 def save(booking):
+    # is_duplicate = check_capacity(booking)
+    # if is_duplicate == True:
+    #     return "error, duplicate bookings are not allowed!!!"
     is_full = check_capacity(booking)
     if is_full == True:
-        return "error, room is full"
+        return "error, room is full!!!"
     
     sql = "INSERT INTO bookings (member_id, lesson_id) VALUES (%s, %s) RETURNING id"
     values = [booking.member.id, booking.lesson.id]
