6b266914f34668b99549cdca8b40050f011fd44b
Jack Dickson <jackowendickson@gmail.com>
2023-07-26 15:00:36 +0100

    changed models to fit sqlAlchemy

--- models/lesson.py
+++ models/lesson.py
@@ -1,4 +1,17 @@
-class Lesson:
+from app import db
+from sqlalchemy.dialects.postgresql import UUID
+import uuid
+
+class Lesson(db.Model):
+    __tablename__ = "lessons"
+    
+    id = db.Column(UUID(as_uuid=True), primary_key = True, default=uuid.uuid4)
+    title = db.Column(db.String(64))
+    capacity = db.Column(db.Integer())
+    lesson_date = db.Column(db.String(64))
+    bookings = db.relationship('Booking', backref="lesson")
+
+
     def __init__(self, title, capacity, lesson_date, id = None):
         self.title = title
         self.capacity = capacity
--- models/member.py
+++ models/member.py
@@ -1,4 +1,14 @@
-class Member:
+from app import db
+from sqlalchemy.dialects.postgresql import UUID
+import uuid
+
+class Member(db.Model):
+    __tablename__ = "members"
+
+    id = db.Column(UUID(as_uuid=True), primary_key = True, default=uuid.uuid4)
+    name = db.Column(db.String(64))
+    bookings = db.relationship('Booking', backref="member")
+
     def __init__(self, name, id = None):
         self.name = name
         self.id = id
