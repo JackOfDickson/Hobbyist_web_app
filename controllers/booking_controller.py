62e4a939fef0c3030407a53229c14af091ec5049
Jack Dickson <jackowendickson@gmail.com>
2023-07-27 15:28:09 +0100

    sorted booking controller

--- controllers/booking_controller.py
+++ controllers/booking_controller.py
@@ -3,31 +3,37 @@ from models.booking import Booking
 import repositories.booking_repository as booking_repository
 import repositories.member_repository as member_repository
 import repositories.lesson_repository as lesson_repository
+from models.lesson import Lesson
+from models.member import Member
+from app import db
 
 bookings_blueprint = Blueprint("bookings", __name__, url_prefix="/bookings")
 
 @bookings_blueprint.route('')
 def bookings():
-    bookings = booking_repository.select_all()
+    bookings = Booking.query.all()
     return render_template("bookings/index.html", bookings = bookings)
 
 @bookings_blueprint.route('/new', methods = ['GET'])
 def new_booking():
-    members = member_repository.select_all()
-    lessons = lesson_repository.select_all()
+    members = Member.query.all()
+    lessons = Lesson.query.all()
     return render_template("bookings/new.html", members = members, lessons = lessons)
 
 @bookings_blueprint.route('/', methods = ['POST'])
 def create_booking():
     member_id = request.form['member_id']
     lesson_id = request.form['lesson_id']
-    member = member_repository.select(member_id)
-    lesson = lesson_repository.select(lesson_id)
+    member = Member.query.get(member_id)
+    lesson = Lesson.query.get(lesson_id)
     booking = Booking(member,lesson)
-    booking_repository.save(booking)
+    db.session.add(booking)
+    db.session.commit()
     return redirect('/bookings')
 
 @bookings_blueprint.route('/<id>/delete')
 def delete_booking(id):
-    booking_repository.delete(id)
+    booking_to_delete = Booking.query.get(id)
+    db.session.delete(booking_to_delete)
+    db.session.commit()
     return redirect('/bookings')
