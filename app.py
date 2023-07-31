46d60a5d6c7fc411e64eeada78c9ea5262d32b47
Jack Dickson <jackowendickson@gmail.com>
2023-07-31 14:33:01 +0100

    had to change the name to hobbyist settings because settings was not being ignored

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from settings import postgresqlUser
+from hobbyist_settings import postgresqlUser
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = f"postgresql://{postgresqlUser}@localhost:5432/hobbyist"
--- settings.py
+++
@@ -1,1 +0,0 @@
-postgresqlUser = "user"
