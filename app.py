39abc3c322b63772adff2d70b6c8cfa2e6163e7f
Jack Dickson <jackowendickson@gmail.com>
2023-07-31 14:35:51 +0100

    my assumption about git and gitignore was wrong, undoing changes done in the last few commits

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from hobbyist_settings import postgresqlUser
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = f"postgresql://{postgresqlUser}@localhost:5432/hobbyist"
+app.config["SQLALCHEMY_DATABASE_URI"] = f"postgresql://user@localhost:5432/hobbyist"
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
--- hobbyist_settings.py
+++
@@ -1,1 +0,0 @@
-postgresUser = "user"
